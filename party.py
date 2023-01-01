0a1f310c80ed7666cd0ceb0b57c7d4f5f2eb213e
tau <ttru702@gmail.com>
2023-01-01 21:39:03 +0900

    add integrated func party_score

--- party.py
+++ party.py
@@ -1,4 +1,3 @@
-import json
 from itertools import combinations
 from collections import defaultdict
 from concurrent import futures
@@ -12,7 +11,7 @@ def party_combinations(top=10, unit=6):
     return list(combinations(df['Self'], unit))
 
 
-def party_score(battle_results, party):
+def get_party_score(battle_results, party):
     print(' | '.join(party))
     party_score = defaultdict(float)
     for pokemon_pty in party:
@@ -22,10 +21,10 @@ def party_score(battle_results, party):
     return (party, party_score)
 
 
-def party_scores(battle_results, parties):
+def get_df_party_scores(battle_results, parties):
     scores = {}
     for party in parties:
-        r = party_score(battle_results, party)
+        r = get_party_score(battle_results, party)
         party_name = '|'.join(r[0])
         scores[party_name] = r[1]
     df = pd.DataFrame.from_dict(data=scores, orient="index")
@@ -33,21 +32,26 @@ def party_scores(battle_results, parties):
     return df
 
 
-def party_scores_multi_process(battle_results, parties, max_workers=4):
+def get_df_party_scores_multi_process(battle_results, parties, max_workers=4):
     parties_splited = list(np.array_split(parties, 4))
     executer = futures.ProcessPoolExecutor(max_workers=max_workers)
-    fts = [executer.submit(party_scores, battle_results, parties) for parties in parties_splited]
+    fts = [executer.submit(get_df_party_scores, battle_results, parties) for parties in parties_splited]
     dfs = [f.result() for f in fts]
     df_score = pd.concat(dfs)
     df_score.insert(0, 'score', df_score.sum(axis=1))
     df_score = df_score.sort_values('score', ascending=False)
-    df_score.to_csv('out/party/score.csv', chunksize=1000)
+    return df_score
+
+
+def party_score(top, unit):
+    p = party_combinations(top, unit)
+    br = pd.read_csv('out/battle_results/avg.csv').set_index('Self').drop('score', axis=1)
+    df_score = get_df_party_scores_multi_process(br, p)
+    df_score.to_csv(f'out/party/score_top{top}_unit{unit}.csv', chunksize=1000)
 
 
 def main() -> None:
-    p = party_combinations(top=50, unit=3)
-    br = pd.read_csv('out/battle_results/avg.csv').set_index('Self').drop('score', axis=1)
-    party_scores_multi_process(br, p)
+    party_score(top=50, unit=3)
 
 
 if __name__ == '__main__':
