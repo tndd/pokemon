cfe2606ae68a22bbcbd1e75aec7fe670ec95891b
tau <ttru702@gmail.com>
2023-01-06 21:53:35 +0900

    add party_ranks
    When runnin on vscode, the program crashes.

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from os import makedirs
 from glob import glob
+from concurrent import futures
 
 from frequency import get_frequency
 
@@ -16,14 +17,24 @@ def get_poke_weight():
 
 def parties_weighted(parties, poke_weight):
     for pokemon, weight in poke_weight.items():
+        print(pokemon)
         parties[pokemon] = parties[pokemon].mul(weight)
     return parties
+
+
+def parties_weighted_multi_process(parties, poke_weight, max_workers=8):
+    chunk_size = max(1, len(parties) // max_workers)
+    parties_splited = [parties[i:i + chunk_size] for i in range(0, len(parties), chunk_size)]
+    with futures.ProcessPoolExecutor(max_workers=max_workers) as executer:
+        fts = [executer.submit(parties_weighted, pty, poke_weight) for pty in parties_splited]
+    dfs = [f.result() for f in fts]
+    return pd.concat(dfs)
 
 
 def party_rank(path):
     parties = pd.read_pickle(path)
     poke_weight = get_poke_weight()
-    parties_w = parties_weighted(parties, poke_weight)
+    parties_w = parties_weighted_multi_process(parties, poke_weight)
     parties_w['score'] = parties_w.sum(axis=1)
     parties_w = parties_w.sort_values(by='score', ascending=False)
     # store parties rank
@@ -33,7 +44,7 @@ def party_rank(path):
     parties_w['score'].to_csv(f'{score_dir}/{filename}.csv')
 
 
-def party_ranks(max_workers=8):
+def party_ranks():
     for path in glob('out/party/*.gz'):
         print(path)
         party_rank(path)
